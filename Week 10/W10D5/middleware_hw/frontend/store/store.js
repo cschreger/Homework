e5f76ab0151eb7a5e06a50316cbf913baf62e0fa
Cade Schreger <cschreger@gmail.com>
2020-07-16 20:08:40 -0400

    Middleware completed w/bonus

--- Week 10/W10D5/middleware_hw/frontend/store/store.js
+++ Week 10/W10D5/middleware_hw/frontend/store/store.js
@@ -1,12 +1,35 @@
-import { createStore } from 'redux';
+//Phase 3:
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
+
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState);
+  //Phase 3:
+  const store = createStore(
+    rootReducer, 
+    preloadedState, 
+    applyMiddleware(addLoggingToDispatch, bonusMiddleware));
+  
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
+
   return store;
 }
 
+const addLoggingToDispatch = store => next => action => {
+  console.log(`Old state: ${store.getState()}`);
+  console.log(action);
+  next(action);
+  console.log(`New state: ${store.getState()}`);
+}
+
+//Bonus Phase:
+const bonusMiddleware = store => next => action => {
+  console.log("I am the bonus middleware")
+  console.log(next)
+}
+
+
 export default configureStore;
+
--- Week 10/W10D5/middleware_hw/frontend/todo_redux.jsx
+++ Week 10/W10D5/middleware_hw/frontend/todo_redux.jsx
@@ -4,11 +4,50 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
+  let store = configureStore(preloadedState);
+
+  //Phase 1:
+  // store.dispatch = addLoggingToDispatch(store);
+
+  //Phase 2:
+  // store = applyMiddlewares(store, addLoggingToDispatch)
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+window.store = configureStore;
+
+//Phase 1: 
+// const addLoggingToDispatch = (store) => {
+//   const origDispatch = store.dispatch;
+//   return (action) => {
+//     console.log(`Old state: ${store.getState()}`);
+//     console.log(action);
+//     origDispatch(action);
+//     console.log(`New state: ${store.getState()}`);
+//   }}
+
+//Phase 2:
+
+  // const addLoggingToDispatch = store => next => action => {
+  //   console.log(`Old state: ${store.getState()}`);
+  //   console.log(action);
+  //   next(action);
+  //   console.log(`New state: ${store.getState()}`);
+  // }
+
+  // const applyMiddlewares = (store, ...middlewares) => {
+  //   let dispatch = store.dispatch;
+
+  //   middlewares.forEach(middleware => {
+  //     dispatch = middleware(store)(dispatch)
+  //   })
+    
+  //   return Object.assign({}, store, { dispatch })
+  // }
+
